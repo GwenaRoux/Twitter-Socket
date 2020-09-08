ac72f21237da92a02458d334d02292dbd43d1e26
John Doe <johndoe@example.com>
2020-09-08 09:42:00 +0200

    Added missing files (whoops)

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const keys = require('./APIKeys');
+const twitterIds = require('./twitterids');
 
 app.use('/source', express.static(__dirname + '/client/source/'));
 
@@ -37,16 +38,35 @@ stream.on('tweet', function (tweet) {
   io.emit('tweet',{ 'tweet': tweet });
 });
 
-var streamuser = Tweet.stream('statuses/filter', { follow: ['2558711971'] });
-
+var streamuser = Tweet.stream('statuses/filter', { follow: [twitterIds.me] });
+////////////////////Store my tweets in the DB and emit the color to mytweets.html/////////////////////////////
+// streamuser.on('tweet', function (tweet) {
+//   console.log(tweet.text)
+//   var tweetdb = new MyTweets(tweet);
+//   tweetdb.save().then(function() {
+//     console.log('Object saved with id : ' + tweetdb.id)
+//   });
+//   io.emit('backgroundcolor',{ 'tweet': tweet.text });
+// });
+///////////////Retweet my tweets/////////////////////
 streamuser.on('tweet', function (tweet) {
-  // console.log(tweet.text)
-  var tweetdb = new MyTweets(tweet);
-  tweetdb.save().then(function() {
-    console.log('Object saved with id : ' + tweetdb.id)
-  });
-  io.emit('backgroundcolor',{ 'tweet': tweet.text });
+  if (!isReply(tweet)) {
+    console.log(tweet);
+    Tweet.post('statuses/retweet/:id', {id: tweet.id_str}, function (err, data, response) {
+      console.log(data)
+    });
+  }
 });
+
+function isReply(tweet) {
+  if ( tweet.retweeted_status
+      || tweet.in_reply_to_status_id
+      || tweet.in_reply_to_status_id_str
+      || tweet.in_reply_to_user_id
+      || tweet.in_reply_to_user_id_str
+      || tweet.in_reply_to_screen_name )
+    return true
+}
 
 ////////////////Api routes/////////////////
 app.get('/', function (req, res) {
@@ -69,18 +89,8 @@ app.get('/api/mytweets', function (req, res) {
     res.json(myTweetsList);
   })
 });
-// 4092632549
-// var bbh = Tweet.stream('statuses/filter', { follow: ['2558711971'] });
-
-streamuser.on('tweet', function (tweet) {
-  console.log(tweet);
-  Tweet.post('statuses/retweet/:id', { id: tweet.id }, function (err, data, response) {
-    console.log(data)
-  });
-});
 
 //////////////Sever start///////////////////
 server.listen(3000, function () {
   console.log('server listening on http://localhost:3000!')
 });
-
